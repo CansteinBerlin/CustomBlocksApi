d2bf060cc29229056fe2c0f702556dfba6c5aadd
Hasenzahn1 <motzer10@gmx.de>
2023-07-10 14:54:03 +0200

    Add some more properties

--- src/main/java/de/canstein_berlin/customblocksapi/api/block/properties/Properties.java
+++ src/main/java/de/canstein_berlin/customblocksapi/api/block/properties/Properties.java
@@ -1,5 +1,6 @@
 package de.canstein_berlin.customblocksapi.api.block.properties;
 
+import org.bukkit.Axis;
 import org.bukkit.block.BlockFace;
 
 import java.util.List;
@@ -10,15 +11,21 @@ import java.util.List;
 public class Properties {
 
     public static DirectionProperty FACING; // NORTH, SOUTH, WEST, EAST
+    public static DirectionProperty DIRECTION; // NORTH, SOUTH, WEST, EAST, UP, DOWN
     public static BooleanProperty POWERED; // true false
     public static BooleanProperty ENABLED; // true, false
     public static BooleanProperty BERRIES; // true, false
+    public static EnumProperty<Axis> AXIS; // x, y, z
+    public static EnumProperty<Axis> HORIZONTAL_AXIS; //x, z
 
     static {
         FACING = new DirectionProperty("facing", List.of(BlockFace.NORTH, BlockFace.SOUTH, BlockFace.WEST, BlockFace.EAST));
+        DIRECTION = new DirectionProperty("direction", List.of(BlockFace.NORTH, BlockFace.SOUTH, BlockFace.WEST, BlockFace.EAST, BlockFace.UP, BlockFace.DOWN));
         POWERED = new BooleanProperty("powered");
         ENABLED = new BooleanProperty("enabled");
         BERRIES = new BooleanProperty("berries");
+        AXIS = new EnumProperty<>("axis", Axis.class, List.of(Axis.X, Axis.Y, Axis.Z));
+        HORIZONTAL_AXIS = new EnumProperty<>("horizontal_axis", Axis.class, List.of(Axis.X, Axis.Z));
     }
 
 }
